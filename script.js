a35ec50b30624dc1d77b13e8ab2dab425f4a8e30
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-07-16 23:16:48 +0200

    Opening and closing form modal

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
+
+// Opening and closing the form modal
+openTaskFormBtn.addEventListener("click", () =>
+    taskForm.classList.toggle("hidden")
+  );
+
+closeTaskFormBtn.addEventListener("click", () => {
+    confirmCloseDialog.showModal(); // display a modal with the Discard and Cancel buttons
+  }); 
+cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
+discardBtn.addEventListener("click", () => {
+    confirmCloseDialog.close();
+    taskForm.classList.toggle("hidden");
+  });
+
+// Get the values from the input fields, save them into the taskData array, and display them on the page
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault(); // Stop the browser from refreshing the page
+    
+});  
+
+// Check if the task being added to the taskData array already exists or not
+const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
